636a0ffe0d2e676396bd75de4ba08d042234f8b8
QingY <qingy735@qq.com>
2024-05-11 21:58:36 +0800

    礼物相关代码

---
+++ qiyu-live-gift-interface/src/main/java/org/qiyu/live/gift/dto/GiftConfigDTO.java
@@ -0,0 +1,15 @@
+package org.qiyu.live.gift.dto;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * @Author idea
+ * @Date: Created in 14:58 2023/7/30
+ * @Description
+ */
+public class GiftConfigDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 2285354775828848375L;
+}
